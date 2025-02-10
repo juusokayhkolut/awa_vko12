c1367c73115cb7a1ded79b4ea988b3757eca1729
Juuso Kayhko <juusookayhko@gmail.com>
2025-02-10 17:52:44 +0200

    Commit

---
+++ client/src/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Books</h1>
+      <h2>404: This is not the webpage you are looking for</h2>
+    </div>
+  )
+}
+
+export default NotFoundPage
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import BookPage from './BookPage'
+import NotFoundPage from './NotFoundPage'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/book/:bookName" element={<BookPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
 )
